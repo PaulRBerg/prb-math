d5630dc056293927cf7fd7fa9f4fa2de50f13730
Paul Razvan Berg <hello@paulrberg.com>
2021-04-17 14:31:49 +0300

    perf: optimise "abs" function

--- contracts/PRBMathSD59x18.sol
+++ contracts/PRBMathSD59x18.sol
@@ -48,8 +48,10 @@ library PRBMathSD59x18 {
     /// @param x The number to calculate the absolute for.
     /// @param result The absolute value of x.
     function abs(int256 x) internal pure returns (int256 result) {
-        require(x > MIN_SD59x18);
-        result = x < 0 ? -x : x;
+        unchecked {
+            require(x > MIN_SD59x18);
+            result = x < 0 ? -x : x;
+        }
     }
 
     /// @notice Calculates arithmetic average of x and y, rounding down.
