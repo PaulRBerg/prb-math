cbc050f2f47824aa0220707179db65c7d396d668
Paul Razvan Berg <hello@paulrberg.com>
2022-04-17 11:51:56 +0300

    perf: unchecked arithmetic in "mostSignificantBit"

--- contracts/PRBMath.sol
+++ contracts/PRBMath.sol
@@ -365,37 +365,39 @@ library PRBMath {
     /// @param x The uint256 number for which to find the index of the most significant bit.
     /// @return msb The index of the most significant bit as an uint256.
     function mostSignificantBit(uint256 x) internal pure returns (uint256 msb) {
-        if (x >= 2**128) {
-            x >>= 128;
-            msb += 128;
-        }
-        if (x >= 2**64) {
-            x >>= 64;
-            msb += 64;
-        }
-        if (x >= 2**32) {
-            x >>= 32;
-            msb += 32;
-        }
-        if (x >= 2**16) {
-            x >>= 16;
-            msb += 16;
-        }
-        if (x >= 2**8) {
-            x >>= 8;
-            msb += 8;
-        }
-        if (x >= 2**4) {
-            x >>= 4;
-            msb += 4;
-        }
-        if (x >= 2**2) {
-            x >>= 2;
-            msb += 2;
-        }
-        if (x >= 2**1) {
+        unchecked {
+            if (x >= 2**128) {
+                x >>= 128;
+                msb += 128;
+            }
+            if (x >= 2**64) {
+                x >>= 64;
+                msb += 64;
+            }
+            if (x >= 2**32) {
+                x >>= 32;
+                msb += 32;
+            }
+            if (x >= 2**16) {
+                x >>= 16;
+                msb += 16;
+            }
+            if (x >= 2**8) {
+                x >>= 8;
+                msb += 8;
+            }
+            if (x >= 2**4) {
+                x >>= 4;
+                msb += 4;
+            }
+            if (x >= 2**2) {
+                x >>= 2;
+                msb += 2;
+            }
             // No need to shift x any more.
-            msb += 1;
+            if (x >= 2**1) {
+                msb += 1;
+            }
         }
     }
 
