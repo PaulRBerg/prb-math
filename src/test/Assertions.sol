7b9271b91193151e3bfecd51660918d90e344552
Paul Razvan Berg <hello@paulrberg.com>
2022-12-13 16:07:42 +0200

    feat: add assertions for array comparisons
    chore: add separators between assertions
    fix: match types for SD1x18 and UD2x18 assertions
    refactor: delete assertions that have an "err" argument

--- src/test/Assertions.sol
+++ src/test/Assertions.sol
@@ -9,99 +9,178 @@ import { UD2x18 } from "../UD2x18.sol";
 import { UD60x18 } from "../UD60x18.sol";
 
 contract Assertions is PRBTest {
+    /*//////////////////////////////////////////////////////////////////////////
+                                       SD1X18
+    //////////////////////////////////////////////////////////////////////////*/
     function assertEq(SD1x18 a, SD1x18 b) internal {
         assertEq(SD1x18.unwrap(a), SD1x18.unwrap(b));
     }
 
+    function assertEq(SD1x18 a, int64 b) internal {
+        assertEq(SD1x18.unwrap(a), b);
+    }
+
+    function assertEq(int64 a, SD1x18 b) internal {
+        assertEq(a, SD1x18.unwrap(b));
+    }
+
+    function assertEq(SD1x18[] memory a, SD1x18[] memory b) internal {
+        int256[] memory castedA;
+        int256[] memory castedB;
+        assembly {
+            castedA := a
+            castedB := b
+        }
+        assertEq(castedA, castedB);
+    }
+
+    function assertEq(SD1x18[] memory a, int64[] memory b) internal {
+        int256[] memory castedA;
+        int256[] memory castedB;
+        assembly {
+            castedA := a
+            castedB := b
+        }
+        assertEq(castedA, castedB);
+    }
+
+    function assertEq(int64[] memory a, SD1x18[] memory b) internal {
+        int256[] memory castedA;
+        int256[] memory castedB;
+        assembly {
+            castedA := a
+            castedB := b
+        }
+        assertEq(castedA, castedB);
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                       SD59X18
+    //////////////////////////////////////////////////////////////////////////*/
+
     function assertEq(SD59x18 a, SD59x18 b) internal {
         assertEq(SD59x18.unwrap(a), SD59x18.unwrap(b));
-    }
-
-    function assertEq(SD1x18 a, SD1x18 b, string memory err) internal {
-        assertEq(SD1x18.unwrap(a), SD1x18.unwrap(b), err);
-    }
-
-    function assertEq(SD59x18 a, SD59x18 b, string memory err) internal {
-        assertEq(SD59x18.unwrap(a), SD59x18.unwrap(b), err);
-    }
-
-    function assertEq(SD1x18 a, int256 b) internal {
-        assertEq(SD1x18.unwrap(a), b);
     }
 
     function assertEq(SD59x18 a, int256 b) internal {
         assertEq(SD59x18.unwrap(a), b);
     }
 
-    function assertEq(SD1x18 a, int256 b, string memory err) internal {
-        assertEq(SD1x18.unwrap(a), b, err);
-    }
-
-    function assertEq(SD59x18 a, int256 b, string memory err) internal {
-        assertEq(SD59x18.unwrap(a), b, err);
-    }
-
-    function assertEq(int256 a, SD1x18 b) internal {
-        assertEq(a, SD1x18.unwrap(b));
-    }
-
     function assertEq(int256 a, SD59x18 b) internal {
         assertEq(a, SD59x18.unwrap(b));
     }
 
-    function assertEq(int256 a, SD1x18 b, string memory err) internal {
-        assertEq(a, SD1x18.unwrap(b), err);
+    function assertEq(SD59x18[] memory a, SD59x18[] memory b) internal {
+        int256[] memory castedA;
+        int256[] memory castedB;
+        assembly {
+            castedA := a
+            castedB := b
+        }
+        assertEq(castedA, castedB);
     }
 
-    function assertEq(int256 a, SD59x18 b, string memory err) internal {
-        assertEq(a, SD59x18.unwrap(b), err);
+    function assertEq(SD59x18[] memory a, int256[] memory b) internal {
+        int256[] memory castedA;
+        assembly {
+            castedA := a
+        }
+        assertEq(castedA, b);
     }
+
+    function assertEq(int256[] memory a, SD59x18[] memory b) internal {
+        int256[] memory castedB;
+        assembly {
+            castedB := b
+        }
+        assertEq(a, b);
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                       UD2X18
+    //////////////////////////////////////////////////////////////////////////*/
 
     function assertEq(UD2x18 a, UD2x18 b) internal {
         assertEq(UD2x18.unwrap(a), UD2x18.unwrap(b));
     }
 
+    function assertEq(UD2x18 a, uint64 b) internal {
+        assertEq(UD2x18.unwrap(a), uint256(b));
+    }
+
+    function assertEq(uint64 a, UD2x18 b) internal {
+        assertEq(uint256(a), UD2x18.unwrap(b));
+    }
+
+    function assertEq(UD2x18[] memory a, UD2x18[] memory b) internal {
+        uint256[] memory castedA;
+        uint256[] memory castedB;
+        assembly {
+            castedA := a
+            castedB := b
+        }
+        assertEq(castedA, castedB);
+    }
+
+    function assertEq(UD2x18[] memory a, uint64[] memory b) internal {
+        uint256[] memory castedA;
+        uint256[] memory castedB;
+        assembly {
+            castedA := a
+            castedB := b
+        }
+        assertEq(castedA, castedB);
+    }
+
+    function assertEq(uint64[] memory a, UD2x18[] memory b) internal {
+        uint256[] memory castedA;
+        uint256[] memory castedB;
+        assembly {
+            castedA := a
+            castedB := b
+        }
+        assertEq(castedA, castedB);
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                       UD60X18
+    //////////////////////////////////////////////////////////////////////////*/
+
     function assertEq(UD60x18 a, UD60x18 b) internal {
         assertEq(UD60x18.unwrap(a), UD60x18.unwrap(b));
-    }
-
-    function assertEq(UD2x18 a, UD2x18 b, string memory err) internal {
-        assertEq(UD2x18.unwrap(a), UD2x18.unwrap(b), err);
-    }
-
-    function assertEq(UD60x18 a, UD60x18 b, string memory err) internal {
-        assertEq(UD60x18.unwrap(a), UD60x18.unwrap(b), err);
-    }
-
-    function assertEq(UD2x18 a, uint256 b) internal {
-        assertEq(UD2x18.unwrap(a), b);
     }
 
     function assertEq(UD60x18 a, uint256 b) internal {
         assertEq(UD60x18.unwrap(a), b);
     }
 
-    function assertEq(UD2x18 a, uint256 b, string memory err) internal {
-        assertEq(UD2x18.unwrap(a), b, err);
-    }
-
-    function assertEq(UD60x18 a, uint256 b, string memory err) internal {
-        assertEq(UD60x18.unwrap(a), b, err);
-    }
-
-    function assertEq(uint256 a, UD2x18 b) internal {
-        assertEq(a, UD2x18.unwrap(b));
-    }
-
     function assertEq(uint256 a, UD60x18 b) internal {
         assertEq(a, UD60x18.unwrap(b));
     }
 
-    function assertEq(uint256 a, UD2x18 b, string memory err) internal {
-        assertEq(a, UD2x18.unwrap(b), err);
+    function assertEq(UD60x18[] memory a, UD60x18[] memory b) internal {
+        uint256[] memory castedA;
+        uint256[] memory castedB;
+        assembly {
+            castedA := a
+            castedB := b
+        }
+        assertEq(castedA, castedB);
     }
 
-    function assertEq(uint256 a, UD60x18 b, string memory err) internal {
-        assertEq(a, UD60x18.unwrap(b), err);
+    function assertEq(UD60x18[] memory a, uint256[] memory b) internal {
+        uint256[] memory castedA;
+        assembly {
+            castedA := a
+        }
+        assertEq(castedA, b);
+    }
+
+    function assertEq(uint256[] memory a, SD59x18[] memory b) internal {
+        uint256[] memory castedB;
+        assembly {
+            castedB := b
+        }
+        assertEq(a, b);
     }
 }
