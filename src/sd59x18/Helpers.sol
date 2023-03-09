612e3f7e3486edd7a3c38ec5358e8ff60f1923c0
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-03-09 17:05:30 +0200

    refactor: use bound "unwrap"

--- src/sd59x18/Helpers.sol
+++ src/sd59x18/Helpers.sol
@@ -1,111 +1,111 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
-import { unwrap, wrap } from "./Casting.sol";
+import { wrap } from "./Casting.sol";
 import { SD59x18 } from "./ValueType.sol";
 
 /// @notice Implements the checked addition operation (+) in the SD59x18 type.
 function add(SD59x18 x, SD59x18 y) pure returns (SD59x18 result) {
-    return wrap(unwrap(x) + unwrap(y));
+    return wrap(x.unwrap() + y.unwrap());
 }
 
 /// @notice Implements the AND (&) bitwise operation in the SD59x18 type.
 function and(SD59x18 x, int256 bits) pure returns (SD59x18 result) {
-    return wrap(unwrap(x) & bits);
+    return wrap(x.unwrap() & bits);
 }
 
 /// @notice Implements the AND (&) bitwise operation in the SD59x18 type.
 function and2(SD59x18 x, SD59x18 y) pure returns (SD59x18 result) {
-    return wrap(unwrap(x) & unwrap(y));
+    return wrap(x.unwrap() & y.unwrap());
 }
 
 /// @notice Implements the equal (=) operation in the SD59x18 type.
 function eq(SD59x18 x, SD59x18 y) pure returns (bool result) {
-    result = unwrap(x) == unwrap(y);
+    result = x.unwrap() == y.unwrap();
 }
 
 /// @notice Implements the greater than operation (>) in the SD59x18 type.
 function gt(SD59x18 x, SD59x18 y) pure returns (bool result) {
-    result = unwrap(x) > unwrap(y);
+    result = x.unwrap() > y.unwrap();
 }
 
 /// @notice Implements the greater than or equal to operation (>=) in the SD59x18 type.
 function gte(SD59x18 x, SD59x18 y) pure returns (bool result) {
-    result = unwrap(x) >= unwrap(y);
+    result = x.unwrap() >= y.unwrap();
 }
 
 /// @notice Implements a zero comparison check function in the SD59x18 type.
 function isZero(SD59x18 x) pure returns (bool result) {
-    result = unwrap(x) == 0;
+    result = x.unwrap() == 0;
 }
 
 /// @notice Implements the left shift operation (<<) in the SD59x18 type.
 function lshift(SD59x18 x, uint256 bits) pure returns (SD59x18 result) {
-    result = wrap(unwrap(x) << bits);
+    result = wrap(x.unwrap() << bits);
 }
 
 /// @notice Implements the lower than operation (<) in the SD59x18 type.
 function lt(SD59x18 x, SD59x18 y) pure returns (bool result) {
-    result = unwrap(x) < unwrap(y);
+    result = x.unwrap() < y.unwrap();
 }
 
 /// @notice Implements the lower than or equal to operation (<=) in the SD59x18 type.
 function lte(SD59x18 x, SD59x18 y) pure returns (bool result) {
-    result = unwrap(x) <= unwrap(y);
+    result = x.unwrap() <= y.unwrap();
 }
 
 /// @notice Implements the unchecked modulo operation (%) in the SD59x18 type.
 function mod(SD59x18 x, SD59x18 y) pure returns (SD59x18 result) {
-    result = wrap(unwrap(x) % unwrap(y));
+    result = wrap(x.unwrap() % y.unwrap());
 }
 
 /// @notice Implements the not equal operation (!=) in the SD59x18 type.
 function neq(SD59x18 x, SD59x18 y) pure returns (bool result) {
-    result = unwrap(x) != unwrap(y);
+    result = x.unwrap() != y.unwrap();
 }
 
 /// @notice Implements the NOT (~) bitwise operation in the SD59x18 type.
 function not(SD59x18 x) pure returns (SD59x18 result) {
-    result = wrap(~unwrap(x));
+    result = wrap(~x.unwrap());
 }
 
 /// @notice Implements the OR (|) bitwise operation in the SD59x18 type.
 function or(SD59x18 x, SD59x18 y) pure returns (SD59x18 result) {
-    result = wrap(unwrap(x) | unwrap(y));
+    result = wrap(x.unwrap() | y.unwrap());
 }
 
 /// @notice Implements the right shift operation (>>) in the SD59x18 type.
 function rshift(SD59x18 x, uint256 bits) pure returns (SD59x18 result) {
-    result = wrap(unwrap(x) >> bits);
+    result = wrap(x.unwrap() >> bits);
 }
 
 /// @notice Implements the checked subtraction operation (-) in the SD59x18 type.
 function sub(SD59x18 x, SD59x18 y) pure returns (SD59x18 result) {
-    result = wrap(unwrap(x) - unwrap(y));
+    result = wrap(x.unwrap() - y.unwrap());
 }
 
 /// @notice Implements the unchecked addition operation (+) in the SD59x18 type.
 function uncheckedAdd(SD59x18 x, SD59x18 y) pure returns (SD59x18 result) {
     unchecked {
-        result = wrap(unwrap(x) + unwrap(y));
+        result = wrap(x.unwrap() + y.unwrap());
     }
 }
 
 /// @notice Implements the unchecked subtraction operation (-) in the SD59x18 type.
 function uncheckedSub(SD59x18 x, SD59x18 y) pure returns (SD59x18 result) {
     unchecked {
-        result = wrap(unwrap(x) - unwrap(y));
+        result = wrap(x.unwrap() - y.unwrap());
     }
 }
 
 /// @notice Implements the unchecked unary minus operation (-) in the SD59x18 type.
 function uncheckedUnary(SD59x18 x) pure returns (SD59x18 result) {
     unchecked {
-        result = wrap(-unwrap(x));
+        result = wrap(-x.unwrap());
     }
 }
 
 /// @notice Implements the XOR (^) bitwise operation in the SD59x18 type.
 function xor(SD59x18 x, SD59x18 y) pure returns (SD59x18 result) {
-    result = wrap(unwrap(x) ^ unwrap(y));
+    result = wrap(x.unwrap() ^ y.unwrap());
 }
