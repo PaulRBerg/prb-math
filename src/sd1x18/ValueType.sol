d25914df0c2fd937ecb001818ea52dcd86919e87
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-03-09 15:46:29 +0200

    refactor: use long names in named imports
    chore: improve formatting in comments

--- src/sd1x18/ValueType.sol
+++ src/sd1x18/ValueType.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
-import "./Casting.sol" as C;
+import "./Casting.sol" as Casting;
 
 /// @notice The signed 1.18-decimal fixed-point number representation, which can have up to 1 digit and up to 18
 /// decimals. The values of this are bound by the minimum and the maximum values permitted by the underlying Solidity
@@ -13,4 +13,12 @@ type SD1x18 is int64;
                                     CASTING
 //////////////////////////////////////////////////////////////////////////*/
 
-using { C.intoSD59x18, C.intoUD2x18, C.intoUD60x18, C.intoUint256, C.intoUint128, C.intoUint40, C.unwrap } for SD1x18 global;
+using {
+    Casting.intoSD59x18,
+    Casting.intoUD2x18,
+    Casting.intoUD60x18,
+    Casting.intoUint256,
+    Casting.intoUint128,
+    Casting.intoUint40,
+    Casting.unwrap
+} for SD1x18 global;
