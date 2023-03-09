dc130054fd994e44bb94d1fdd8ebd8e142bc2b3a
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-03-09 15:46:29 +0200

    refactor: use long names in named imports
    chore: improve formatting in comments

--- src/sd59x18/ValueType.sol
+++ src/sd59x18/ValueType.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19;
 
-import "./Casting.sol" as C;
-import "./Helpers.sol" as H;
-import "./Math.sol" as M;
+import "./Casting.sol" as Casting;
+import "./Helpers.sol" as Helpers;
+import "./Math.sol" as Math;
 
 /// @notice The signed 59.18-decimal fixed-point number representation, which can have up to 59 digits and up to 18
 /// decimals. The values of this are bound by the minimum and the maximum values permitted by the underlying Solidity
@@ -15,14 +15,14 @@ type SD59x18 is int256;
 //////////////////////////////////////////////////////////////////////////*/
 
 using {
-    C.intoInt256,
-    C.intoSD1x18,
-    C.intoUD2x18,
-    C.intoUD60x18,
-    C.intoUint256,
-    C.intoUint128,
-    C.intoUint40,
-    C.unwrap
+    Casting.intoInt256,
+    Casting.intoSD1x18,
+    Casting.intoUD2x18,
+    Casting.intoUD60x18,
+    Casting.intoUint256,
+    Casting.intoUint128,
+    Casting.intoUint40,
+    Casting.unwrap
 } for SD59x18 global;
 
 /*//////////////////////////////////////////////////////////////////////////
@@ -30,23 +30,23 @@ using {
 //////////////////////////////////////////////////////////////////////////*/
 
 using {
-    M.abs,
-    M.avg,
-    M.ceil,
-    M.div,
-    M.exp,
-    M.exp2,
-    M.floor,
-    M.frac,
-    M.gm,
-    M.inv,
-    M.log10,
-    M.log2,
-    M.ln,
-    M.mul,
-    M.pow,
-    M.powu,
-    M.sqrt
+    Math.abs,
+    Math.avg,
+    Math.ceil,
+    Math.div,
+    Math.exp,
+    Math.exp2,
+    Math.floor,
+    Math.frac,
+    Math.gm,
+    Math.inv,
+    Math.log10,
+    Math.log2,
+    Math.ln,
+    Math.mul,
+    Math.pow,
+    Math.powu,
+    Math.sqrt
 } for SD59x18 global;
 
 /*//////////////////////////////////////////////////////////////////////////
@@ -54,46 +54,46 @@ using {
 //////////////////////////////////////////////////////////////////////////*/
 
 using {
-    H.add,
-    H.and,
-    H.eq,
-    H.gt,
-    H.gte,
-    H.isZero,
-    H.lshift,
-    H.lt,
-    H.lte,
-    H.mod,
-    H.neq,
-    H.not,
-    H.or,
-    H.rshift,
-    H.sub,
-    H.uncheckedAdd,
-    H.uncheckedSub,
-    H.uncheckedUnary,
-    H.xor
+    Helpers.add,
+    Helpers.and,
+    Helpers.eq,
+    Helpers.gt,
+    Helpers.gte,
+    Helpers.isZero,
+    Helpers.lshift,
+    Helpers.lt,
+    Helpers.lte,
+    Helpers.mod,
+    Helpers.neq,
+    Helpers.not,
+    Helpers.or,
+    Helpers.rshift,
+    Helpers.sub,
+    Helpers.uncheckedAdd,
+    Helpers.uncheckedSub,
+    Helpers.uncheckedUnary,
+    Helpers.xor
 } for SD59x18 global;
 
 /*//////////////////////////////////////////////////////////////////////////
                                     OPERATORS
 //////////////////////////////////////////////////////////////////////////*/
 
-/// The global "using for" directive makes it possible to use these operators on the SD59x18 type.
+// The global "using for" directive makes it possible to use these operators on the SD59x18 type.
 using {
-    H.add as +,
-    H.and2 as &,
-    M.div as /,
-    H.eq as ==,
-    H.gt as >,
-    H.gte as >=,
-    H.lt as <,
-    H.lte as <=,
-    H.mod as %,
-    M.mul as *,
-    H.neq as !=,
-    H.not as ~,
-    H.or as |,
-    H.sub as -,
-    H.xor as ^
+    Helpers.add as +,
+    Helpers.and2 as &,
+    Math.div as /,
+    Helpers.eq as ==,
+    Helpers.gt as >,
+    Helpers.gte as >=,
+    Helpers.lt as <,
+    Helpers.lte as <=,
+    Helpers.mod as %,
+    Math.mul as *,
+    Helpers.neq as !=,
+    Helpers.not as ~,
+    Helpers.or as |,
+    Helpers.sub as -,
+    Helpers.xor as ^
 } for SD59x18 global;
--- src/ud60x18/ValueType.sol
+++ src/ud60x18/ValueType.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19;
 
-import "./Casting.sol" as C;
-import "./Helpers.sol" as H;
-import "./Math.sol" as M;
+import "./Casting.sol" as Casting;
+import "./Helpers.sol" as Helpers;
+import "./Math.sol" as Math;
 
 /// @notice The unsigned 60.18-decimal fixed-point number representation, which can have up to 60 digits and up to 18
 /// decimals. The values of this are bound by the minimum and the maximum values permitted by the Solidity type uint256.
@@ -14,77 +14,85 @@ type UD60x18 is uint256;
                                     CASTING
 //////////////////////////////////////////////////////////////////////////*/
 
-using { C.intoSD1x18, C.intoUD2x18, C.intoSD59x18, C.intoUint128, C.intoUint256, C.intoUint40, C.unwrap } for UD60x18 global;
+using {
+    Casting.intoSD1x18,
+    Casting.intoUD2x18,
+    Casting.intoSD59x18,
+    Casting.intoUint128,
+    Casting.intoUint256,
+    Casting.intoUint40,
+    Casting.unwrap
+} for UD60x18 global;
 
 /*//////////////////////////////////////////////////////////////////////////
                             MATHEMATICAL FUNCTIONS
 //////////////////////////////////////////////////////////////////////////*/
 
-/// The global "using for" directive makes the functions in this library callable on the UD60x18 type.
+// The global "using for" directive makes the functions in this library callable on the UD60x18 type.
 using {
-    M.avg,
-    M.ceil,
-    M.div,
-    M.exp,
-    M.exp2,
-    M.floor,
-    M.frac,
-    M.gm,
-    M.inv,
-    M.ln,
-    M.log10,
-    M.log2,
-    M.mul,
-    M.pow,
-    M.powu,
-    M.sqrt
+    Math.avg,
+    Math.ceil,
+    Math.div,
+    Math.exp,
+    Math.exp2,
+    Math.floor,
+    Math.frac,
+    Math.gm,
+    Math.inv,
+    Math.ln,
+    Math.log10,
+    Math.log2,
+    Math.mul,
+    Math.pow,
+    Math.powu,
+    Math.sqrt
 } for UD60x18 global;
 
 /*//////////////////////////////////////////////////////////////////////////
                                 HELPER FUNCTIONS
 //////////////////////////////////////////////////////////////////////////*/
 
-/// The global "using for" directive makes the functions in this library callable on the UD60x18 type.
+// The global "using for" directive makes the functions in this library callable on the UD60x18 type.
 using {
-    H.add,
-    H.and,
-    H.eq,
-    H.gt,
-    H.gte,
-    H.isZero,
-    H.lshift,
-    H.lt,
-    H.lte,
-    H.mod,
-    H.neq,
-    H.not,
-    H.or,
-    H.rshift,
-    H.sub,
-    H.uncheckedAdd,
-    H.uncheckedSub,
-    H.xor
+    Helpers.add,
+    Helpers.and,
+    Helpers.eq,
+    Helpers.gt,
+    Helpers.gte,
+    Helpers.isZero,
+    Helpers.lshift,
+    Helpers.lt,
+    Helpers.lte,
+    Helpers.mod,
+    Helpers.neq,
+    Helpers.not,
+    Helpers.or,
+    Helpers.rshift,
+    Helpers.sub,
+    Helpers.uncheckedAdd,
+    Helpers.uncheckedSub,
+    Helpers.xor
 } for UD60x18 global;
 
 /*//////////////////////////////////////////////////////////////////////////
                                     OPERATORS
 //////////////////////////////////////////////////////////////////////////*/
 
-/// The global "using for" directive makes it possible to use these operators on the UD60x18 type.
+// The global "using for" directive makes it possible to use these operators on the UD60x18 type.
 using {
-    H.add as +,
-    H.and2 as &,
-    M.div as /,
-    H.eq as ==,
-    H.gt as >,
-    H.gte as >=,
-    H.lt as <,
-    H.lte as <=,
-    H.or as |,
-    H.mod as %,
-    M.mul as *,
-    H.neq as !=,
-    H.not as ~,
-    H.sub as -,
-    H.xor as ^
+    Helpers.add as +,
+    Helpers.and2 as &,
+    Math.div as /,
+    Helpers.eq as ==,
+    Helpers.gt as >,
+    Helpers.gte as >=,
+    Helpers.lt as <,
+    Helpers.lte as <=,
+    Helpers.or as |,
+    Helpers.mod as %,
+    Math.mul as *,
+    Helpers.neq as !=,
+    Helpers.not as ~,
+    Helpers.sub as -,
+    Helpers.xor as ^
 } for UD60x18 global;
