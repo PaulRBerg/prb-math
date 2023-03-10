366f014a4b9c72a1d70624998d6d6bcee46cc25e
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-03-10 10:22:22 +0200

    chore: rewrite me

--- src/Common.sol
+++ src/Common.sol
@@ -10,16 +10,16 @@ pragma solidity >=0.8.13;
                                 CUSTOM ERRORS
 //////////////////////////////////////////////////////////////////////////*/
 
-/// @notice Thrown when the ending result in {mulDiv} would overflow uint256.
+/// @notice Thrown when the ending result in {mulDivFloor} would overflow uint256.
 error PRBMath_MulDiv_Overflow(uint256 x, uint256 y, uint256 denominator);
 
-/// @notice Thrown when the ending result in the fixed-point version of {mulDiv} would overflow uint256.
+/// @notice Thrown when the ending result in the fixed-point version of {mulDivFloor} would overflow uint256.
 error PRBMath_MulDiv18_Overflow(uint256 x, uint256 y);
 
-/// @notice Thrown when attempting to run {mulDiv} with one of the inputs `type(int256).min`.
+/// @notice Thrown when attempting to run {mulDivFloor} with one of the inputs `type(int256).min`.
 error PRBMath_MulDivSigned_InputTooSmall();
 
-/// @notice Thrown when the ending result in the signed version of {mulDiv} would overflow int256.
+/// @notice Thrown when the ending result in the signed version of {mulDivFloor} would overflow int256.
 error PRBMath_MulDivSigned_Overflow(int256 x, int256 y);
 
 /*//////////////////////////////////////////////////////////////////////////
@@ -361,7 +361,7 @@ function msb(uint256 x) pure returns (uint256 result) {
     }
 }
 
-/// @notice Calculates floor(x*y÷denominator) with full precision.
+/// @notice Calculates floor(x*y÷denominator) with full precision, rounding down.
 ///
 /// @dev Credits to Remco Bloemen under MIT license https://xn--2-umb.com/21/muldiv.
 ///
@@ -376,7 +376,7 @@ function msb(uint256 x) pure returns (uint256 result) {
 /// @param y The multiplier as an uint256.
 /// @param denominator The divisor as an uint256.
 /// @return result The result as an uint256.
-function mulDiv(uint256 x, uint256 y, uint256 denominator) pure returns (uint256 result) {
+function mulDivFloor(uint256 x, uint256 y, uint256 denominator) pure returns (uint256 result) {
     // 512-bit multiply [prod1 prod0] = x * y. Compute the product mod 2^256 and mod 2^256 - 1, then use
     // use the Chinese Remainder Theorem to reconstruct the 512 bit result. The result is stored in two 256
     // variables such that product = prod1 * 2^256 + prod0.
@@ -429,7 +429,7 @@ function mulDiv(uint256 x, uint256 y, uint256 denominator) pure returns (uint256
             // Divide [prod1 prod0] by lpotdod.
             prod0 := div(prod0, lpotdod)
 
-            // Get the flipped value `2^256 / lpotdod`. If the `lpotdod` is zero, the flipped value is one.
+            // Compute the flipped value `2^256 / lpotdod`. If the `lpotdod` is zero, the flipped value is one.
             // `sub(0, lpotdod)` produces the two's complement version of `lpotdod`, which is equivalent to flipping all the bits.
             // However, `div` interprets this value as an unsigned value: https://ethereum.stackexchange.com/q/147168/24693
             flippedLpotdod := add(div(sub(0, lpotdod), lpotdod), 1)
@@ -460,9 +460,112 @@ function mulDiv(uint256 x, uint256 y, uint256 denominator) pure returns (uint256
     }
 }
 
+function mulDivCeil(uint256 x, uint256 y, uint256 denominator) pure returns (uint256 result) {
+    // 512-bit multiply [prod1 prod0] = x * y. Compute the product mod 2^256 and mod 2^256 - 1, then use
+    // use the Chinese Remainder Theorem to reconstruct the 512 bit result. The result is stored in two 256
+    // variables such that product = prod1 * 2^256 + prod0.
+    uint256 prod0; // Least significant 256 bits of the product
+    uint256 prod1; // Most significant 256 bits of the product
+    assembly ("memory-safe") {
+        let mm := mulmod(x, y, not(0))
+        prod0 := mul(x, y)
+        prod1 := sub(sub(mm, prod0), lt(mm, prod0))
+    }
+
+    // Make sure the result is less than 2^256. Also prevents denominator == 0.
+    if (prod1 >= denominator) {
+        revert PRBMath_MulDiv_Overflow(x, y, denominator);
+    }
+    // => In the rest of this function, denominator >=1
+
+    // Handle non-overflow cases, 256 by 256 division.
+    if (prod1 == 0) {
+        unchecked {
+            result = prod0 / denominator; // = floor(prod0 / denominator)
+            if (
+                result * denominator == prod0 // multiplication cannot overflow and is cheaper then computing MULMOD opcode
+            ) {
+                // here denominator divides prod0
+                return result;
+            } else {
+                // here denominator does not divides prod0. In particular we must have denominator >= 2. We want to return
+                // ceil(prod0 / denominator), so we must add one.
+                return result + 1; // result <=floor(MAX_UINT256/denominator)<==floor(MAX_UINT256/2)<MAX_UINT256, so adding one
+                    // cannot cause overflow
+            }
+        }
+    }
+
+    ///////////////////////////////////////////////
+    // 512 by 256 division.
+    ///////////////////////////////////////////////
+
+    // Make division exact by subtracting the remainder from [prod1 prod0].
+    uint256 remainder;
+    assembly ("memory-safe") {
+        // Compute remainder using the mulmod Yul instruction.
+        remainder := mulmod(x, y, denominator)
+
+        // Subtract 256 bit number from 512 bit number.
+        prod1 := sub(prod1, gt(remainder, prod0))
+        prod0 := sub(prod0, remainder)
+    }
+
+    // Factor powers of two out of denominator and compute largest power of two divisor of denominator. Always >= 1.
+    // See https://cs.stackexchange.com/q/138556/92363.
+    unchecked {
+        // Does not overflow because the denominator cannot be zero at this stage in the function.
+        uint256 lpotdod = denominator & (~denominator + 1);
+        assembly ("memory-safe") {
+            // Divide denominator by lpotdod.
+            denominator := div(denominator, lpotdod)
+
+            // Divide [prod1 prod0] by lpotdod.
+            prod0 := div(prod0, lpotdod)
+
+            // Flip lpotdod such that it is 2^256 / lpotdod. If lpotdod is zero, then it becomes one.
+            lpotdod := add(div(sub(0, lpotdod), lpotdod), 1)
+        }
+
+        // Shift in bits from prod1 into prod0.
+        prod0 |= prod1 * lpotdod;
+
+        // Invert denominator mod 2^256. Now that denominator is an odd number, it has an inverse modulo 2^256 such
+        // that denominator * inv = 1 mod 2^256. Compute the inverse by starting with a seed that is correct for
+        // four bits. That is, denominator * inv = 1 mod 2^4.
+        uint256 inverse = (3 * denominator) ^ 2;
+
+        // Use the Newton-Raphson iteration to improve the precision. Thanks to Hensel's lifting lemma, this also works
+        // in modular arithmetic, doubling the correct bits in each step.
+        inverse *= 2 - denominator * inverse; // inverse mod 2^8
+        inverse *= 2 - denominator * inverse; // inverse mod 2^16
+        inverse *= 2 - denominator * inverse; // inverse mod 2^32
+        inverse *= 2 - denominator * inverse; // inverse mod 2^64
+        inverse *= 2 - denominator * inverse; // inverse mod 2^128
+        inverse *= 2 - denominator * inverse; // inverse mod 2^256
+
+        // Because the division is now exact we can divide by multiplying with the modular inverse of denominator.
+        // This will give us the correct result modulo 2^256. Since the preconditions guarantee that the outcome is
+        // less than 2^256, this is the final result. We don't need to compute the high bits of the result and prod1
+        // is no longer required.
+        result = prod0 * inverse;
+    }
+    assembly ("memory-safe") {
+        result := add(result, iszero(iszero(remainder))) // iszero(iszero(remainder)) returns 0 if remainder==0 and 1 otherwise
+            // Thus, the line above causes overflow iff ((remainder != 0) and (result == MAX_UINT256)) in which case the resulting
+            // value of result after this line is zero.
+    }
+    // Remark: we know that prod1 != 0 => x,y !=0. Thus we can have result == 0 only if we had an overflow.
+    if (result == 0) {
+        revert PRBMath_MulDiv_Overflow(x, y, denominator);
+    } else {
+        return result;
+    }
+}
+
 /// @notice Calculates floor(x*y÷1e18) with full precision.
 ///
-/// @dev A variant of {mulDiv} with constant folding, i.e. in which the denominator is always 1e18. Before returning the
+/// @dev A variant of {mulDivFloor} with constant folding, i.e. in which the denominator is always 1e18. Before returning the
 /// final result, we add 1 if `(x * y) % UNIT >= HALF_UNIT`. Without this adjustment, 6.6e-19 would be truncated to 0
 /// instead of being rounded to 1e-18. See "Listing 6" and text above it at https://accu.org/index.php/journals/1717.
 ///
@@ -470,7 +573,7 @@ function mulDiv(uint256 x, uint256 y, uint256 denominator) pure returns (uint256
 /// - The result must fit within uint256.
 ///
 /// Notes:
-/// - The body is purposely left uncommented; to understand how this works, see the NatSpec comments in {mulDiv}.
+/// - The body is purposely left uncommented; to understand how this works, see the NatSpec comments in {mulDivFloor}.
 /// - It is assumed that the result can never be `type(uint256).max` when x and y solve the following two equations:
 ///     1. x * y = type(uint256).max * UNIT
 ///     2. (x * y) % UNIT >= UNIT / 2
@@ -513,7 +616,7 @@ function mulDiv18(uint256 x, uint256 y) pure returns (uint256 result) {
 
 /// @notice Calculates floor(x*y÷denominator) with full precision.
 ///
-/// @dev An extension of {mulDiv} for signed numbers. Works by computing the signs and the absolute values separately.
+/// @dev An extension of {mulDivFloor} for signed numbers. Works by computing the signs and the absolute values separately.
 ///
 /// Requirements:
 /// - None of the inputs can be `type(int256).min`.
@@ -539,7 +642,7 @@ function mulDivSigned(int256 x, int256 y, int256 denominator) pure returns (int2
     }
 
     // Compute the absolute value of (x*y)÷denominator. The result must fit within int256.
-    uint256 resultAbs = mulDiv(xAbs, yAbs, dAbs);
+    uint256 resultAbs = mulDivFloor(xAbs, yAbs, dAbs);
     if (resultAbs > uint256(type(int256).max)) {
         revert PRBMath_MulDivSigned_Overflow(x, y);
     }
@@ -560,6 +663,68 @@ function mulDivSigned(int256 x, int256 y, int256 denominator) pure returns (int2
     // If there are, the result should be negative. Otherwise, it should be positive.
     unchecked {
         result = sx ^ sy ^ sd == 0 ? -int256(resultAbs) : int256(resultAbs);
+    }
+}
+
+/// @notice Calculates floor(x*y÷denominator) with full precision.
+///
+/// @dev An extension of `mulDivFloor` for signed numbers. Works by computing the signs and the absolute values separately.
+///
+/// Requirements:
+/// - None of the inputs can be `type(int256).min`.
+/// - The result must fit within int256.
+///
+/// @param x The multiplicand as an int256.
+/// @param y The multiplier as an int256.
+/// @param denominator The divisor as an int256.
+/// @return result The result as an int256.
+function mulDivFloorSigned(int256 x, int256 y, int256 denominator) pure returns (int256) {
+    if (x == type(int256).min || y == type(int256).min || denominator == type(int256).min) {
+        revert PRBMath_MulDivSigned_InputTooSmall();
+    }
+
+    // Get the signs of x, y and the denominator.
+    uint256 sx;
+    uint256 sy;
+    uint256 sd;
+    assembly ("memory-safe") {
+        // This works thanks to two's complement.
+        // "sgt" stands for "signed greater than" and "sub(0,1)" is max uint256.
+        sx := sgt(x, sub(0, 1))
+        sy := sgt(y, sub(0, 1))
+        sd := sgt(denominator, sub(0, 1))
+    }
+
+    // Get hold of the absolute values of x, y and the denominator.
+    uint256 absX;
+    uint256 absY;
+    uint256 absD;
+    unchecked {
+        absX = x < 0 ? uint256(-x) : uint256(x);
+        absY = y < 0 ? uint256(-y) : uint256(y);
+        absD = denominator < 0 ? uint256(-denominator) : uint256(denominator);
+    }
+    uint256 rAbs;
+    // XOR over sx, sy and sd. What this does is to check whether there are 1 or 3 negative signs in the inputs.
+    // If there are, the result should be negative. Otherwise, it should be positive.
+    if (sx ^ sy ^ sd == 0) {
+        rAbs = mulDivCeil(absX, absY, absD); // Compute the absolute value of ceil(x*y)÷denominator.
+        if (rAbs > uint256(type(int256).max)) {
+            // The result must fit within int256.
+            revert PRBMath_MulDivSigned_Overflow(x, y);
+        }
+        unchecked {
+            return -int256(rAbs);
+        }
+    } else {
+        rAbs = mulDivFloor(absX, absY, absD); // Compute the absolute value of floor(x*y)÷denominator.
+        if (rAbs > uint256(type(int256).max)) {
+            // The result must fit within int256.
+            revert PRBMath_MulDivSigned_Overflow(x, y);
+        }
+        unchecked {
+            return int256(rAbs);
+        }
     }
 }
 
