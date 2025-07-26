b5a3accb055810198c890cfef2fddfda7fc62f49
Tiago de Paula <tiagodepalves@gmail.com>
2025-07-26 13:01:27 -0300

    perf: branchless square root implementation

--- test/fuzz/common/msb.t.sol
+++ test/fuzz/common/msb.t.sol
@@ -5,6 +5,51 @@ import { msb } from "src/Common.sol";
 
 import { Base_Test } from "../../Base.t.sol";
 
+/// @dev Previous implementation, for verifying regressions.
+///
+/// From https://github.com/PaulRBerg/prb-math/blob/v4.1.0/src/Common.sol#L297-L372
+function originalMsb(uint256 x) pure returns (uint256 result) {
+    assembly ("memory-safe") {
+        let factor := shl(7, gt(x, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF))
+        x := shr(factor, x)
+        result := or(result, factor)
+    }
+    assembly ("memory-safe") {
+        let factor := shl(6, gt(x, 0xFFFFFFFFFFFFFFFF))
+        x := shr(factor, x)
+        result := or(result, factor)
+    }
+    assembly ("memory-safe") {
+        let factor := shl(5, gt(x, 0xFFFFFFFF))
+        x := shr(factor, x)
+        result := or(result, factor)
+    }
+    assembly ("memory-safe") {
+        let factor := shl(4, gt(x, 0xFFFF))
+        x := shr(factor, x)
+        result := or(result, factor)
+    }
+    assembly ("memory-safe") {
+        let factor := shl(3, gt(x, 0xFF))
+        x := shr(factor, x)
+        result := or(result, factor)
+    }
+    assembly ("memory-safe") {
+        let factor := shl(2, gt(x, 0xF))
+        x := shr(factor, x)
+        result := or(result, factor)
+    }
+    assembly ("memory-safe") {
+        let factor := shl(1, gt(x, 0x3))
+        x := shr(factor, x)
+        result := or(result, factor)
+    }
+    assembly ("memory-safe") {
+        let factor := gt(x, 0x1)
+        result := or(result, factor)
+    }
+}
+
 /// @dev Collection of tests for the most significant bit function available in `Common.sol`.
 contract Common_Sqrt_Test is Base_Test {
     function testFuzz_Msb_FitsUint8(uint256 x) external pure {
@@ -31,5 +76,9 @@ contract Common_Sqrt_Test is Base_Test {
 
     function testFuzz_Msb_Shifts2ToMoreThan(uint256 x) external pure whenShiftLeftDoesNotOverflow(x) {
         assertGt(2 << msb(x), x, "Common msb");
+    }
+
+    function testFuzz_Msb_MatchesOriginalImplementation(uint256 x) external pure {
+        assertEq(msb(x), originalMsb(x), "Common msb");
     }
 }
--- test/fuzz/common/sqrt.t.sol
+++ test/fuzz/common/sqrt.t.sol
@@ -5,6 +5,60 @@ import { sqrt } from "src/Common.sol";
 
 import { Base_Test } from "../../Base.t.sol";
 
+/// @dev Previous implementation, for verifying regressions.
+///
+/// From https://github.com/PaulRBerg/prb-math/blob/v4.1.0/src/Common.sol#L587-L675
+function originalSqrt(uint256 x) pure returns (uint256 result) {
+    if (x == 0) {
+        return 0;
+    }
+
+    uint256 xAux = uint256(x);
+    result = 1;
+    if (xAux >= 2 ** 128) {
+        xAux >>= 128;
+        result <<= 64;
+    }
+    if (xAux >= 2 ** 64) {
+        xAux >>= 64;
+        result <<= 32;
+    }
+    if (xAux >= 2 ** 32) {
+        xAux >>= 32;
+        result <<= 16;
+    }
+    if (xAux >= 2 ** 16) {
+        xAux >>= 16;
+        result <<= 8;
+    }
+    if (xAux >= 2 ** 8) {
+        xAux >>= 8;
+        result <<= 4;
+    }
+    if (xAux >= 2 ** 4) {
+        xAux >>= 4;
+        result <<= 2;
+    }
+    if (xAux >= 2 ** 2) {
+        result <<= 1;
+    }
+
+    unchecked {
+        result = (result + x / result) >> 1;
+        result = (result + x / result) >> 1;
+        result = (result + x / result) >> 1;
+        result = (result + x / result) >> 1;
+        result = (result + x / result) >> 1;
+        result = (result + x / result) >> 1;
+        result = (result + x / result) >> 1;
+
+        uint256 roundedResult = x / result;
+        if (result >= roundedResult) {
+            result = roundedResult;
+        }
+    }
+}
+
 /// @dev Collection of tests for the square root function available in `Common.sol`.
 contract Common_Sqrt_Test is Base_Test {
     uint256 internal constant MAX_SQRT = type(uint128).max;
@@ -43,5 +97,9 @@ contract Common_Sqrt_Test is Base_Test {
 
     function testFuzz_Sqrt_OfPowerOfTwo(uint8 x) external pure whenEven(x) {
         vm.assertEq(sqrt(2 ** x), 2 ** (x / 2), "Common sqrt");
+    }
+
+    function testFuzz_Sqrt_MatchesOriginalImplementation(uint256 x) external pure {
+        assertEq(sqrt(x), originalSqrt(x), "Common sqrt");
     }
 }
